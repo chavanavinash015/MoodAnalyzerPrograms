7c84f0f068326994cdf0b9043d9f0dc0f842252b
arc-015 <chavanavinash015@gmail.com>
2022-11-10 21:48:23 +0530

    Added Mood Analyser test Sad or Happy

--- src/test/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerTest.java
@@ -14,16 +14,15 @@ public class MoodAnalyzerTest {
     public void afterDisplayMethod(){
         System.out.println("test is passed");
     }
-     @Test
-     void givenMessgae_WhenSad_ShouldReturn_Sad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Sad Message");
+   @Test
+    public void givenMessage_WhenSad_ShouldReturn_Sad(){
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is Sad Message");
         String mood1 = moodAnalyzer.analyzeMood();
         Assertions.assertEquals("SAD",mood1);
-
     }
 
     @Test
-    void givenMessage_WhenNotSad_ShouldReturn_Happy() {
+    public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is Happy Messgae");
         String mood2 = moodAnalyzer.analyzeMood();
         Assertions.assertEquals("HAPPY",mood2);
